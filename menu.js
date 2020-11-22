8fbf1fb6a2ee149f2e265a66b2e06a26586fbe32
hipolitonamburete <hnamburete@gmail.com>
2020-11-22 03:15:08 -0800

    Primeiro commit

---
+++ menu.js
@@ -0,0 +1,24 @@
+/*
+var btn = document.getElementById(btn-demo);
+//var container = document.querySelector(".demo");
+var container = document.getElementById(demo);
+
+btn.addEventListener(onclick, hide(container));
+
+function hide(){
+
+       alert("Vejo a div");
+  
+    /*
+  if(container.style.display == "block") {
+      container.style.display = "none";
+  } else {
+      container.style.display = "block";
+  }*/
+
+  function hide(){
+      var ocultar = document.getElementById(demo);
+      ocultar.style.display = 'none';
+      alert("feito");
+
+  }
